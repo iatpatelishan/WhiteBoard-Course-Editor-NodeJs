127f796c55a9471f3d08ec084e7d5ed07a28e4d1
Ishan Patel <i@patelishan.com>
2018-06-28 21:52:22 -0400

    Allow access from everywhere :x #Testing

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(function (req, res, next) {
     var origin = req.headers.origin;
     if(allowedOrigins.indexOf(origin) > -1){
         res.header('Access-Control-Allow-Origin', origin);
-    }
+    } 
     res.header("Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods",
