57a5e21389863686a718a394f11bac6a091d6b7c
Ishan Patel <i@patelishan.com>
2018-06-28 21:48:48 -0400

    Allow access from everywhere :x #Testing

--- server.js
+++ server.js
@@ -10,8 +10,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin",
-        "http://localhost:4200");
+    var allowedOrigins = ['http://127.0.0.1:4200', 'http://cs5610-angular-patel.herokuapp.com', 'https://cs5610-angular-patel.herokuapp.com'];
+    var origin = req.headers.origin;
+    if(allowedOrigins.indexOf(origin) > -1){
+        res.header('Access-Control-Allow-Origin', origin);
+    }
     res.header("Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods",
